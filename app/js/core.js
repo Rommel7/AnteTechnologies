c40f12b020be8bf8aeafac505f97b0fba9ac1b8c
Rommel7 <ramil.mamedov7991@gmail.com>
2018-04-27 21:11:36 +0400

    finished without responsive

--- app/js/core.js
+++ app/js/core.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   console.log('Ready!');
 
-    
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
 });
